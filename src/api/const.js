1ff73ae7ad750b0ee92cd629055ec0dccae0f599
no3utufff89 <14198908@mail.rugit config --global core.autocrlf truegit config --global core.safecrlf warngit config --global core.quotepath offgit add .git statusgit config --global user.email 14198908@mail.ru>
2023-08-31 22:49:48 +0300

    deploy2

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'YIyx-QiudQ5MV57TAKtmWw';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'https://no3utufff89.github.io/blogget';
+export const REDIRECT_URI = 'https://no3utufff89.github.io/blogget/index.html';
 export const SCOPE = 'identity submit read';
